8ea10108d91a6988c62188863cd15f4b403a0c67
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-06-07 06:56:40 -0500

    agegando las rutas de terceros y pedidos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorNoEncontradoComponent } from './components/error-no-encontrado/error-no-encontrado.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { TercerosComponent } from './components/terceros/terceros.component';
 
 const routes: Routes = [
   { path: 'iniciar-sesion', component: IniciarSesionComponent },
+  { path: 'tercero', component: TercerosComponent },
+  { path: 'pedido', component: PedidosComponent },
   { path: '', redirectTo: 'iniciar-sesion', pathMatch: 'full' },
   { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
 ];
@@ -13,4 +17,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
